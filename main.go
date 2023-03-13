158b6d9c060a65171af7f810e97528178e936c99
Pakornpat Sinjiranon <skyfox.ku@gmail.com>
2023-03-13 22:28:09 +0700

    experiment post and get image

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"io"
 	"log"
 	"net/http"
 	"os"
@@ -8,6 +9,8 @@ import (
 
 	"github.com/julienschmidt/httprouter"
 )
+
+var image []byte
 
 type application struct {
 	logger *log.Logger
@@ -17,9 +20,38 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("it's works"))
 }
 
+func (app *application) uploadImageHandler(w http.ResponseWriter, r *http.Request) {
+	f, fh, err := r.FormFile("image")
+	if err != nil {
+		app.logger.Printf("%v\n", err)
+		return
+	}
+	defer f.Close()
+	app.logger.Printf("%v\n", fh.Header)
+
+	fb, err := io.ReadAll(f)
+	if err != nil {
+		app.logger.Printf("%v\n", err)
+		return
+	}
+	image = fb
+}
+
+func (app *application) downloadImageHandler(w http.ResponseWriter, r *http.Request) {
+	if image == nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/octet-stream")
+	w.Write(image)
+}
+
 func (app *application) routes() *httprouter.Router {
 	router := httprouter.New()
 	router.HandlerFunc(http.MethodGet, "/", app.home)
+	router.HandlerFunc(http.MethodGet, "/image", app.downloadImageHandler)
+	router.HandlerFunc(http.MethodPost, "/image", app.uploadImageHandler)
 	return router
 }
 
